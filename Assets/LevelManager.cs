28921e2b069e505ed44d278664feee939fe95986
Jernej Petaros <jernej.petaros@razum.si>
2017-01-20 13:55:48 +0100

    * Implemented the LevelManager * Implemented Start and Quit button logic

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public void LoadLevel(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+
+    public void QuitRequest()
+    {
+        Application.Quit();
+    }
+}
